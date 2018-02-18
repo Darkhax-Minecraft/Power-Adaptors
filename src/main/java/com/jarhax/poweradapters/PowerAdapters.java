9d7c7f0612eaa25bb85c791f551767dbbd6c9778
Tyler Hancock <darklime@live.ca>
2018-02-17 23:16:08 -0700

    Added main mod class

---
+++ src/main/java/com/jarhax/poweradapters/PowerAdapters.java
@@ -0,0 +1,8 @@
+package com.jarhax.poweradapters;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "poweradapters", name = "Power Adapters", version = "@VERSION@", dependencies = "", certificateFingerprint = "@FINGERPRINT@")
+public class PowerAdapters {
+
+}
