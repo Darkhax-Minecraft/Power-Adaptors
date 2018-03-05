e4e817716786e7a005be894144651537bdb80757
Tyler Hancock <darklime@live.ca>
2018-03-05 15:48:26 -0700

    Fixed internal battery not saving/reading correctly.

--- src/main/java/com/jarhax/poweradapters/InternalBattery.java
+++ src/main/java/com/jarhax/poweradapters/InternalBattery.java
@@ -4,101 +4,114 @@ import net.minecraft.nbt.NBTTagCompound;
 
 public class InternalBattery {
 
-	private long stored;
-	
-	private long capacity;
-	
-	private long input;
-	
-	private long output;
-	
-	public InternalBattery(NBTTagCompound tag) {
-		
-		read(tag);
-	}
-	
-	public InternalBattery(InternalBattery original) {
-		
-		this(original.getStored(), original.getCapacity(), original.getInput(), original.getOutput());
-	}
-	
-	public InternalBattery(InternalBattery original, long capacity, long input, long output) {
-		
-		this(original.getStored(), capacity, input, output);
-	}
-	
-	public InternalBattery(long capacity, long input, long output) {
-		
-		this(0, capacity, input, output);
-	}
-	
-	public InternalBattery(long stored, long capacity, long input, long output) {
-		
-		this.stored = stored;
-		this.capacity = capacity;
-		this.input = input;
-		this.output = output;
-	}
-	
-	public void write(NBTTagCompound tag) {
-		
-		NBTTagCompound data = new NBTTagCompound();
-		data.setLong("Stored", this.getStored());
-		data.setLong("Capacity", this.getCapacity());
-		data.setLong("Input", this.getInput());
-		data.setLong("Output", this.getOutput());
-		tag.setTag("InternalBattery", data);
-	}
-	
-	public void read(NBTTagCompound tag) {
-		
-		NBTTagCompound data = tag.getCompoundTag("InternalBatter");
-		this.setStored(data.getLong("Stored"));
-		this.setCapacity(data.getLong("Capacity"));
-		this.setInput(data.getLong("Input"));
-		this.setInput(data.getLong("Ouput"));
-	}
+    public static final String TAG_STORED = "Stored";
+    public static final String TAG_CAPACITY = "Capacity";
+    public static final String TAG_INPUT = "Input";
+    public static final String TAG_OUTPUT = "Output";
+    public static final String TAG_BATTERY = "InternalBattery";
     
-    
-    public void takePower(long power) {
-        
-        this.setStored(Math.min(this.getCapacity(), this.getStored() - power));
+    private long stored;
+
+    private long capacity;
+
+    private long input;
+
+    private long output;
+
+    public InternalBattery (NBTTagCompound tag) {
+
+        this.read(tag);
+    }
+
+    public InternalBattery (InternalBattery original) {
+
+        this(original.getStored(), original.getCapacity(), original.getInput(), original.getOutput());
+    }
+
+    public InternalBattery (InternalBattery original, long capacity, long input, long output) {
+
+        this(original.getStored(), capacity, input, output);
+    }
+
+    public InternalBattery (long capacity, long input, long output) {
+
+        this(0, capacity, input, output);
+    }
+
+    public InternalBattery (long stored, long capacity, long input, long output) {
+
+        this.stored = stored;
+        this.capacity = capacity;
+        this.input = input;
+        this.output = output;
     }
     
-	public void addPower(long power) {
-		
-		this.setStored(Math.min(this.getCapacity(), this.getStored() + power));
-	}
-	
-	public long getStored() {
-		return stored;
-	}
+    public void write (NBTTagCompound tag) {
 
-	public void setStored(long stored) {
-		this.stored = stored;
-	}
+        final NBTTagCompound data = new NBTTagCompound();
+        data.setLong(TAG_STORED, this.getStored());
+        data.setLong(TAG_CAPACITY, this.getCapacity());
+        data.setLong(TAG_INPUT, this.getInput());
+        data.setLong(TAG_OUTPUT, this.getOutput());
+        tag.setTag(TAG_BATTERY, data);
+    }
 
-	public long getCapacity() {
-		return capacity;
-	}
+    public void read (NBTTagCompound tag) {
 
-	public void setCapacity(long capacity) {
-		this.capacity = capacity;
-	}
+        final NBTTagCompound data = tag.getCompoundTag(TAG_BATTERY);
+        this.setStored(data.getLong(TAG_STORED));
+        this.setCapacity(data.getLong(TAG_CAPACITY));
+        this.setInput(data.getLong(TAG_INPUT));
+        this.setInput(data.getLong(TAG_OUTPUT));
+    }
 
-	public long getInput() {
-		return input;
-	}
+    public void takePower (long power) {
 
-	public void setInput(long input) {
-		this.input = input;
-	}
+        this.setStored(Math.min(this.getCapacity(), this.getStored() - power));
+    }
 
-	public long getOutput() {
-		return output;
-	}
+    public void addPower (long power) {
 
-	public void setOutput(long output) {
-		this.output = output;
-	}
+        this.setStored(Math.min(this.getCapacity(), this.getStored() + power));
+    }
+
+    public long getStored () {
+
+        return this.stored;
+    }
+
+    public void setStored (long stored) {
+
+        this.stored = stored;
+    }
+
+    public long getCapacity () {
+
+        return this.capacity;
+    }
+
+    public void setCapacity (long capacity) {
+
+        this.capacity = capacity;
+    }
+
+    public long getInput () {
+
+        return this.input;
+    }
+
+    public void setInput (long input) {
+
+        this.input = input;
+    }
+
+    public long getOutput () {
+
+        return this.output;
+    }
+
+    public void setOutput (long output) {
+
+        this.output = output;
+    }
 }
